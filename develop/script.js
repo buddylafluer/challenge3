9198f30efa4c2b604acc34cdd40cfba6f3cbd206
buddy <buddylafluer@gmail.com>
2022-09-16 01:01:18 -0400

    added variables

--- develop/script.js
+++ develop/script.js
@@ -1,4 +1,13 @@
-// Assignment code here
+// Defining variables
+var ConfirmUpperCase;
+var ConfirmLowerCase;
+var ConfirmNumbers;
+var ConfirmSpecialCharacters;
+
+let upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "=", "?", ">", "<", "/"];
+let numbers = ["1","2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 
 // Get references to the #generate element
